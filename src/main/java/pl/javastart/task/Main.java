edafda664c470bb32d0db052f57d2ae3e5fcb477
Bartek Krawczyk <bartoszkrawczykk@gmail.com>
2023-01-31 18:47:45 +0100

    Rozwiązanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,5 +4,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+            String name1 = "Mleko";
+            String name2 = "Czekolada";
+            String producer1 = "Mlekowita";
+            String producer2 = "Wedel";
+            double price1 = 2.5;
+            double price2 = 2.19;
+
+        System.out.printf("%s %s %.1fzł%n", name1, producer1, price1);
+        System.out.printf("%s %s %.2fzł", name2, producer2, price2);
     }
 }
